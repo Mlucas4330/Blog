505005c118c2e76e3c8fdefd69c1de440c1472d4
Lucas Medeiros <mlucas4330@gmail.com>
2023-08-04 01:13:11 -0300

    new test

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*"
+    origins "https://blog-v4.onrender.com", "http://localhost:3000"
 
     resource "*",
       headers: :any,
