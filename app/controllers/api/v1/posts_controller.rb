de38beed7c75514d1bb393881aa3e0262824abd1
Lucas Medeiros <mlucas4330@gmail.com>
2023-08-04 02:02:54 -0300

    debug

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -24,7 +24,7 @@ end
 
 def create
   authorize()
-
+  return
   render json: @user.inspect
   return 
   @post = @user.posts.new(post_params)
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,6 +26,7 @@ class ApplicationController < ActionController::API
     end
 
     def authorize
-        render json: {message: 'Você precisa estar logado'}, status: :unauthorized unless authorized_user
+        render json: authorized_user
+        # render json: {message: 'Você precisa estar logado'}, status: :unauthorized unless authorized_user
     end
 end
