1142e158ab70d95cf86a614a1bc5a6bb722aac98
Lucas Medeiros <mlucas4330@gmail.com>
2023-08-04 05:21:27 -0300

    last try

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::API
+    JWT_SECRET_KEY = 'bc8b5443fe37bae5c2d7b0f183d6f669'
     def encode_token(payload)
-        JWT.encode(payload, ENV['JWT_SECRET_KEY'])
+        JWT.encode(payload, JWT_SECRET_KEY)
     end
 
     def decode_token
@@ -8,28 +9,23 @@ class ApplicationController < ActionController::API
 
         if auth_header
             token = auth_header.split(' ').last
-
             begin
-                JWT.decode(token, ENV['JWT_SECRET_KEY'], true, algorithm: 'HS256')
+                JWT.decode(token, JWT_SECRET_KEY, true, algorithm: 'HS256')
             rescue JWT::DecodeError => e
-                { errors: e.message }
+                nil
             end
-        else
-            { errors: 'Authorization header format is invalid' }
+
         end
     end
 
     def authorized_user
-        decoded_token = decode_token()
-        if decoded_token[:errors]
-            render json: decoded_token
-        else
-            user_id = decoded_token[0]['user_id']
+        if decode_token
+            user_id = decode_token.first['user_id']
             @user = User.find_by(id: user_id)
         end
     end
 
     def authorize
-        render json: {message: 'Você precisa estar logado'}, status: :unauthorized unless authorized_user
+        render json: {message: 'You must log in'}, status: :unauthorized unless authorized_user
     end
 end
